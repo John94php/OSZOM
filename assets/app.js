ab06b26e8167bc023b82e77200879563f253fdac
John94php <janzalesinski@gmail.com>
2023-07-06 17:09:22 +0200

    add bootstrap + vue

--- assets/app.js
+++ assets/app.js
@@ -9,5 +9,5 @@ import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
+import './styles/global.scss'
 registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
